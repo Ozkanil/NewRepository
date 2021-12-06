28d04c6b8f8e40addc4640520033fb41617fef02
Ozkanil <ozkanilhan@gmail.com>
2021-12-06 18:56:37 +0300

    first commit

---
+++ src/gitHub/MyFirstClass.java
@@ -0,0 +1,10 @@
+package gitHub;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first class");
+
+	}
+
+}
---
+++ src/gitHub/MySecondClass.java
@@ -0,0 +1,9 @@
+package gitHub;
+
+public class MySecondClass {
+
+	public static void main(String[] args) {
+System.out.println("This is my second class");
+	}
+
+}
